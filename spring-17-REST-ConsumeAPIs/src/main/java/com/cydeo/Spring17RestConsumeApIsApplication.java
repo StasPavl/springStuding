1b02061dd6797f3d6cc7625d455481406e108a06
stanislavpavlovskyi <lichfb@gmail.com>
2023-05-29 10:42:48 -0400

    REST Consuming (RESTTemplate)

--- spring-17-REST-ConsumeAPIs/src/main/java/com/cydeo/Spring17RestConsumeApIsApplication.java
+++ spring-17-REST-ConsumeAPIs/src/main/java/com/cydeo/Spring17RestConsumeApIsApplication.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17RestConsumeApIsApplication {
@@ -10,4 +12,8 @@ public class Spring17RestConsumeApIsApplication {
         SpringApplication.run(Spring17RestConsumeApIsApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
